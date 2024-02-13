b3331c3eaf6418830759d48f78f8bfdfaa663da2
Guillaume Brunet <gbrunet@student.42.fr>
2024-02-13 14:31:56 +0100

    ex00 update

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,11 +6,25 @@
 /*   By: gbrunet <gbrunet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 08:45:13 by gbrunet           #+#    #+#             */
-/*   Updated: 2024/02/01 08:51:13 by gbrunet          ###   ########.fr       */
+/*   Updated: 2024/02/13 14:23:13 by gbrunet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+
+void	to_upper(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			std::cout << (char)(str[i] + 'A' - 'a');
+		else
+			std::cout << (char)str[i];
+	}
+}
 
 int	main(int ac, char **av)
 {
@@ -19,7 +33,7 @@ int	main(int ac, char **av)
 	else
 	{
 		for (int i = 1; i < ac; i++)
-			std::cout << av[i];
+			to_upper(av[i]);
 		std::cout << std::endl;
 	}
 	return (0);
