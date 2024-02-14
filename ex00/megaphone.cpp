bed73bb345d319a5d1ae2bf2661ba1aba9114a48
Guillaume Brunet <gbrunet@student.42.fr>
2024-02-14 10:09:15 +0100

    ex00 update

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,34 +6,24 @@
 /*   By: gbrunet <gbrunet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 08:45:13 by gbrunet           #+#    #+#             */
-/*   Updated: 2024/02/13 14:23:13 by gbrunet          ###   ########.fr       */
+/*   Updated: 2024/02/14 10:07:50 by gbrunet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-void	to_upper(char *str)
-{
-	int	i;
-
-	i = -1;
-	while (str[++i])
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			std::cout << (char)(str[i] + 'A' - 'a');
-		else
-			std::cout << (char)str[i];
-	}
-}
+#include <cstring>
 
 int	main(int ac, char **av)
 {
+	int	j;
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (int i = 1; i < ac; i++)
-			to_upper(av[i]);
+	else {
+		for (int i = 1; i < ac; i++) {
+			j = -1;
+			while (av[i][++j])
+				std::cout << (char)std::toupper(av[i][j]);
+		}
 		std::cout << std::endl;
 	}
 	return (0);
